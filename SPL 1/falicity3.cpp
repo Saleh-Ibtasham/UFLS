4c3ef8ca03b336988bd7fc445c91bc1d0c047b01
Your Name <you@example.com>
2018-05-10 18:01:07 +0600

    commit 18

--- SPL 1/falicity3.cpp
+++ SPL 1/falicity3.cpp
@@ -1,5 +1,8 @@
-#Include <iostream>
+#include <iostream>
 #include <fstream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -12,6 +15,10 @@ vector <int> *allocatedSites;
 
 double *totalCostOfFacility;
 int *demandPoint_is_allocated;
+vector <int> chosen_facility;
+int *checked_facility;
+int optimum_is_changed = 0;
+double optimum_value = pow(2,63) - 10;
 
 void makemat()
 {
@@ -58,29 +65,131 @@ void rankFacilities()
     sort(ranked_list.begin(), ranked_list.end());
 }
 
+void printFacilities()
+{
+    cout << "The facilities chosen: " <<endl;
+    for(int i=0; i<chosen_facility.size(); i++)
+        cout << chosen_facility[i]+1 << endl;
+    cout << endl;
+}
+
+double checkVal(int temporary[], int clusterOfFacility)
+{
+    double sum = 0;
+    checked_facility = new int[clusterOfFacility];
+    for(int i=0; i<clusterOfFacility; i++)
+        checked_facility[i] = 0;
+    for(int i=0; i<numberOfDemandPoints; i++)
+    {
+        int minimum = 100000000;
+        int x = -1;
+        for(int j=0; j<clusterOfFacility; j++)
+            if(costMat[i][temporary[j]] < minimum)
+            {
+                minimum = costMat[i][temporary[j]];
+                x=j;
+            }
+        if(x>-1)
+            checked_facility[x] = 1;
+        sum += minimum;
+    }
+
+    for(int i=0; i<clusterOfFacility; i++)
+        if(checked_facility[i] == 1)
+            sum+= allocationCostMat[temporary[i]];
+
+    return sum;
+}
+
+void combinationCompute(int temporary[], int firstEntry, int lastEntry, int index, int clusterOfFacility, int &combination_tolerance)
+{
+    if(index == clusterOfFacility)
+    {
+        double value = checkVal(temporary,clusterOfFacility);
+        if(optimum_value > value)
+        {
+            chosen_facility.clear();
+            optimum_value = value;
+            for(int i=0; i<clusterOfFacility; i++)
+                if(checked_facility[i] == 1)
+                    chosen_facility.push_back(temporary[i]);
+            if(combination_tolerance)
+                combination_tolerance--;
+            optimum_is_changed = 1;
+        }
+        else
+            combination_tolerance++;
+        delete [] checked_facility;
+        return;
+    }
+    for(int i=firstEntry; (i<=lastEntry) && ((lastEntry-i+1) >= (clusterOfFacility-index)); i++)
+    {
+        temporary[index] = ranked_list[i].second;
+        combinationCompute(temporary, i+1, lastEntry, index+1, clusterOfFacility, combination_tolerance);
+        if(combination_tolerance > 10)
+        {
+            combination_tolerance = 0;
+            break;
+        }
+    }
+    return;
+}
+
 void computeOptimumValue()
 {
-    int n = numberOfFaclity/2;
-    int stage_tolerance = 0;
-    double optimum_value = pow(2,63) - 10;
-    int numberOfValidFacility = 0;
+    int n = numberOfFacility/2;
+    int group_tolerance = 0, combination_tolerance = 0;
     for(int i=1; i<=n; i++)
     {
-        double data[i];
-        double present_value = combinationUtility(data,0,numberOfFacility-1,0,i);
-        if(present_value < optimum_value)
+        combination_tolerance = 0;
+        int temporary[i];
+        combinationCompute(temporary,0,numberOfFacility-1,0,i,combination_tolerance);
+
+        if(optimum_is_changed)
         {
-            optimum_value = present_value;
-            numberOfValidFacility = i;
+            if(group_tolerance)
+                group_tolerance--;
+            optimum_is_changed = 0;
         }
+        else
+            group_tolerance++;
+        if(i == 10)
+            break;
+        if(group_tolerance == 5)
+            break;
     }
 }
 
+void printOptimum()
+{
+    cout << "The Optimum Value is " << optimum_value << endl;
+    cout << endl;
+}
+
+void printDemandPointAssignments()
+{
+    for(int i=0; i<numberOfDemandPoints; i++)
+    {
+        int minimum = 100000000;
+        int x=-1;
+        for(int j=0; j<chosen_facility.size(); j++)
+            if(costMat[i][chosen_facility[j]] < minimum)
+            {
+                minimum = costMat[i][chosen_facility[j]];
+                x = j;
+            }
+        cout << "Demand Point " << i+1 << " will be served by facility " << chosen_facility[x]+1 <<endl;
+    }
+}
 int main()
 {
     readfile();
     rankFacilities();
+    /*for(int i=0; i<numberOfFacility; i++)
+        cout << ranked_list[i].first << " " << ranked_list[i].second << endl;*/
     computeOptimumValue();
-
+    printFacilities();
+    printOptimum();
+    printDemandPointAssignments();
     return 0;
 }
